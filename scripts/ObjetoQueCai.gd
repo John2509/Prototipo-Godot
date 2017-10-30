52b56bd582799d09eec68d84a6e336ea715f5a03
John2509 <jmarcelofo@gmail.com>
2017-10-30 10:38:19 -0200

    Objeto que cai implementado
    Objeto que cai feito completamente por enquanto, ainda falta terminar o tiro

--- scripts/ObjetoQueCai.gd
+++ scripts/ObjetoQueCai.gd
@@ -1,27 +1,25 @@
-extends KinematicBody2D
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-const GRAVITY = 1500.0 # Pixels/second
-const IS_FALLING = 0 #se esta caindo, pode matar
-var velocity = 0
-var speed = 0
+extends Node2D
 
+#var IS_FALLING = 0 #se esta caindo, pode matar
+
+#func _input(event):
+#	print("nao apertou")
+#	#IS_FALLING = event.is_action_pressed("drop_all") #checar se alguem interagiu com um dos objetos
+#	if (event.is_action_pressed("drop_all")):
+#		print("apertou")
+#		set_mode(2)
 
 func _fixed_process(delta):
-	_mover(delta)
-	pass
-	
-func _mover(delta):
-	if(IS_FALLING):
-		if(not is_colliding()):
-				speed += GRAVITY * delta
-				velocity = speed * delta
-				move(Vector2 (0,velocity))
-	else:
-		IS_FALLING = Input.is_action_pressed("drop_all") #checar se alguem interagiu com um dos objetos
-	pass
-	
+	if (ativar()):
+		for filho in get_children():
+			filho.set_sleeping(false)
+
+func ativar():
+	return Input.is_action_pressed("drop_all")
+
 func _ready():
+	for filho in get_children():
+		print(filho.get_name())
+		filho.set_sleeping(true)
 	set_fixed_process(true)
 	pass
