a1ca7bfc04675ecab081b6900cf73fdcad47a872
John2509 <jmarcelofo@gmail.com>
2017-10-30 17:04:44 -0200

    No geral, melhorias no prototipo
    Adicionei os sprites do theo, modulei mais as senas, corrigi um bug da interacao do objeto que cai e o cenario

--- scripts/ObjetoQueCai.gd
+++ scripts/ObjetoQueCai.gd
@@ -2,24 +2,27 @@ extends Node2D
 
 #var IS_FALLING = 0 #se esta caindo, pode matar
 
-#func _input(event):
-#	print("nao apertou")
-#	#IS_FALLING = event.is_action_pressed("drop_all") #checar se alguem interagiu com um dos objetos
-#	if (event.is_action_pressed("drop_all")):
-#		print("apertou")
-#		set_mode(2)
-
 func _fixed_process(delta):
+	#print ("ok")
 	if (ativar()):
 		for filho in get_children():
-			filho.set_sleeping(false)
+			if (filho.get_type() == "RigidBody2D"):
+				filho.set_sleeping(false)
+				filho.set_gravity_scale(6)
+		#print ("nao ok")
 
 func ativar():
 	return Input.is_action_pressed("drop_all")
 
 func _ready():
+	print("teste")
 	for filho in get_children():
-		print(filho.get_name())
-		filho.set_sleeping(true)
+		if (filho.get_type() == "RigidBody2D"):
+			filho.set_sleeping(true)
+			print(filho.get_name())
+			print(filho.is_sleeping())
+			print(filho.get_mode())
+			filho.set_mode(0)
+			filho.set_gravity_scale(0)
 	set_fixed_process(true)
 	pass
