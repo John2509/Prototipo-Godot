8830d626018852ddd900d3dd22343b3d873e043c
John2509 <jmarcelofo@gmail.com>
2017-10-31 15:21:02 -0200

    Comandos de debug comentados
    Tinha comandos de print que nao estavam comentados

--- scripts/ObjetoQueCai.gd
+++ scripts/ObjetoQueCai.gd
@@ -15,13 +15,13 @@ func ativar():
 	return Input.is_action_pressed("drop_all")
 
 func _ready():
-	print("teste")
+	#print("teste")
 	for filho in get_children():
 		if (filho.get_type() == "RigidBody2D"):
 			filho.set_sleeping(true)
-			print(filho.get_name())
-			print(filho.is_sleeping())
-			print(filho.get_mode())
+			#print(filho.get_name())
+			#print(filho.is_sleeping())
+			#print(filho.get_mode())
 			filho.set_mode(0)
 			filho.set_gravity_scale(0)
 	set_fixed_process(true)
