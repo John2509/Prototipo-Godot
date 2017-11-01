2545afcee6974d8b70fd1e2faf57e4590f924a04
John2509 <jmarcelofo@gmail.com>
2017-11-01 01:20:13 -0200

    Bola tem limite de tempo

--- scripts/bala.gd
+++ scripts/bala.gd
@@ -2,15 +2,19 @@ extends KinematicBody2D
 
 var direcao = Vector2()
 var colidiu = false
+var time
 const velocidade = 600
+const TIME_MAX = 0.3
 
 func _ready():
 	set_fixed_process(true)
 	set_rot(direcao.angle())
+	time = 0.0
 
 func _fixed_process(delta):
-	if (not is_colliding() and not colidiu):
+	if (not is_colliding() and not colidiu and time < TIME_MAX) :
 		move(direcao * delta * velocidade)
+		time += delta
 	else:
 		queue_free()
 
