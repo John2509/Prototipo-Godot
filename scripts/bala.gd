8d5791282b371f4507f74b40cbba6b8044f2a4b0
theosato <matheussato10@gmail.com>
2017-11-01 01:25:48 -0200

    Add files via upload

--- scripts/bala.gd
+++ scripts/bala.gd
@@ -6,17 +6,25 @@ var time
 const velocidade = 600
 const TIME_MAX = 0.3
 
+var new_animation = ""
+var animation = ""
+
 func _ready():
 	set_fixed_process(true)
 	set_rot(direcao.angle())
 	time = 0.0
 
 func _fixed_process(delta):
+	new_animation = "fading" 
 	if (not is_colliding() and not colidiu and time < TIME_MAX) :
 		move(direcao * delta * velocidade)
 		time += delta
 	else:
 		queue_free()
+		
+	if animation != new_animation:
+		get_node("AnimationBall").play(new_animation)
+		animation = new_animation
 
 func set_direcao(dir):
 	direcao = dir
